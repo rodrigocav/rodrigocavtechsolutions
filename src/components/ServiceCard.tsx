f961c6864647d6c5bb3b4e90224bfadf4f4c0636
Rodrigo <rodrigo.cavicchioli@outlook.com>
2026-03-24 23:40:55 -0300

    Corrigido bug do card

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -32,11 +32,12 @@ export default function ServiceCard({ title, description, image }: Props) {
   return (
     <div ref={ref} className={styles.card}>
       <div>
+        <img src={image} alt={title} className={styles.image} />
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
 
-      <img src={image} alt={title} className={styles.image} />
+
     </div>
   );
 }
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,20 +9,20 @@ import systemcalcpis from "../assets/systemcalcpis.png";
 export const projects = [
   {
     id: 1,
+    image: siteempresa,
     title: "Site Profissional",
     description: "Tenha um site moderno que transmite confiança e ajuda a atrair novos clientes todos os dias.",
-    image: siteempresa,
   },
   {
+    image: systempythonaut,
     id: 2,
     title: "Automação de Processos",
     description: "Pare de perder tempo com tarefas manuais e automatize seu negócio para ganhar produtividade.",
-    image: systempythonaut,
   },
   {
     id: 3,
+    image: systemcalcpis,
     title: "Automação de Impostos",
     description: "Evite erros e automatize o cálculo de impostos como PIS e IPI com mais segurança e rapidez.",
-    image: systemcalcpis,
   },
 ];
